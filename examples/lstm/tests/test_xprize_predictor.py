4291f2604ae505aaece32588f4ac476a92dcda47
Olivier Francon <olivier.francon@cognizant.com>
2020-09-24 19:16:29 +0200

    #3 Copy Oxford dataset locally

--- examples/lstm/tests/test_xprize_predictor.py
+++ examples/lstm/tests/test_xprize_predictor.py
@@ -8,7 +8,7 @@ from ..xprize_predictor import XPrizePredictor
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 FIXTURES_PATH = os.path.join(ROOT_DIR, 'fixtures')
 EXAMPLE_INPUT_FILE = os.path.join(ROOT_DIR, "../../../2020-08-01_2020-08-04_npis_example.csv")
-DATA_URL = "https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv"
+DATA_FILE = os.path.join(ROOT_DIR, "../data/OxCGRT_latest.csv")
 PREDICTOR_27 = os.path.join(FIXTURES_PATH, "pred27", "predictor.h5")
 PREDICTOR_30 = os.path.join(FIXTURES_PATH, "pred30", "predictor.h5")
 PREDICTOR_31 = os.path.join(FIXTURES_PATH, "pred31", "predictor.h5")
@@ -24,11 +24,11 @@ END_DATE = "2020-08-04"
 class TestMultiplicativeEvaluator(unittest.TestCase):
 
     def test_predict(self):
-        predictor = XPrizePredictor(PREDICTOR_31, DATA_URL, CUTOFF_DATE)
+        predictor = XPrizePredictor(PREDICTOR_31, DATA_FILE, CUTOFF_DATE)
         pred_df = predictor._predict(START_DATE, END_DATE, EXAMPLE_INPUT_FILE)
         self.assertIsInstance(pred_df, pd.DataFrame)
 
     def test_train(self):
-        predictor = XPrizePredictor(None, DATA_URL, CUTOFF_DATE)
+        predictor = XPrizePredictor(None, DATA_FILE, CUTOFF_DATE)
         model = predictor.train()
         self.assertIsNotNone(model)
