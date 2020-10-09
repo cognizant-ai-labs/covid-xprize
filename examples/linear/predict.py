d08411f81007f1b068d997802010286db677377d
Olivier Francon <olivier.francon@cognizant.com>
2020-10-09 17:08:41 +0200

    #45 Move the prediction logic to prediction.py and comply with the prediction API

--- examples/linear/predict.py
+++ examples/linear/predict.py
@@ -1,13 +1,33 @@
 import argparse
 import os
+import pickle
 
+import numpy as np
 import pandas as pd
 
-from examples.lstm.xprize_predictor import XPrizePredictor
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_FILE = os.path.join(ROOT_DIR, "models.pkl")
 DATA_FILE = os.path.join(ROOT_DIR, 'data', "OxCGRT_latest.csv")
+ID_COLS = ['CountryName',
+           'RegionName',
+           'GeoID',
+           'Date']
+CASES_COL = ['NewCases']
+NPI_COLS = ['C1_School closing',
+            'C2_Workplace closing',
+            'C3_Cancel public events',
+            'C4_Restrictions on gatherings',
+            'C5_Close public transport',
+            'C6_Stay at home requirements',
+            'C7_Restrictions on internal movement',
+            'C8_International travel controls',
+            'H1_Public information campaigns',
+            'H2_Testing policy',
+            'H3_Contact tracing']
+NB_LOOKBACK_DAYS = 30
+# For testing, restrict training data to that before a hypothetical predictor submission date
+HYPOTHETICAL_SUBMISSION_DATE = np.datetime64("2020-07-31")
 
 
 def predict(start_date: str,
@@ -26,10 +46,107 @@ def predict(start_date: str,
     with columns "CountryName,RegionName,Date,PredictedDailyNewCases"
     """
     # !!! YOUR CODE HERE !!!
+    preds_df = predict_df(start_date, end_date, path_to_ips_file)
+    # Create the output path
+    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
     # Save to a csv file
-    output_file = start_date + "_" + end_date + ".csv"
-    preds_df.to_csv(output_file, index=False)
-    print(f"Saved predictions to {output_file}")
+    preds_df.to_csv(output_file_path, index=False)
+    print(f"Saved predictions to {output_file_path}")
+
+
+def predict_df(start_date_str: str, end_date_str: str, path_to_ips_file: str):
+    """
+    Generates a file with daily new cases predictions for the given countries, regions and npis, between
+    start_date and end_date, included.
+    :param start_date_str: day from which to start making predictions, as a string, format YYYY-MM-DDD
+    :param end_date_str: day on which to stop making predictions, as a string, format YYYY-MM-DDD
+    :param path_to_ips_file: path to a csv file containing the intervention plans between start_date and end_date
+    :return: a Pandas DataFrame containing the predictions
+    """
+    start_date = pd.to_datetime(start_date_str, format='%Y-%m-%d')
+    end_date = pd.to_datetime(end_date_str, format='%Y-%m-%d')
+
+    # Load the intervention plans
+    ips_df = pd.read_csv(path_to_ips_file,
+                         parse_dates=['Date'],
+                         encoding="ISO-8859-1",
+                         error_bad_lines=True)
+
+    # Add RegionID column that combines CountryName and RegionName for easier manipulation of data\n",
+    ips_df['GeoID'] = ips_df['CountryName'] + '__' + ips_df['RegionName'].astype(str)
+
+    # Copy the test data frame
+    pred_df = ips_df[ID_COLS].copy()
+    # Keep only the requested prediction period.
+    # Note: this period *might* be in the future, and pred_df doesn't necessarily contain the requested rows
+    pred_df = pred_df[(pred_df.Date >= start_date) & (pred_df.Date <= end_date)]
+
+    # Load historical data to use in making predictions in the same way
+    # This is the data we trained on
+    # We stored it locally as for predictions there will be no access to the internet
+    hist_df = pd.read_csv(DATA_FILE,
+                          parse_dates=['Date'],
+                          encoding="ISO-8859-1",
+                          error_bad_lines=False)
+    # Add RegionID column that combines CountryName and RegionName for easier manipulation of data
+    hist_df['GeoID'] = hist_df['CountryName'] + '__' + hist_df['RegionName'].astype(str)
+    # Add new cases column
+    hist_df['NewCases'] = hist_df.groupby('GeoID').ConfirmedCases.diff().fillna(0)
+    # Fill any missing case values by interpolation and setting NaNs to 0
+    hist_df.update(hist_df.groupby('GeoID').NewCases.apply(
+        lambda group: group.interpolate()).fillna(0))
+    # Fill any missing NPIs by assuming they are the same as previous day
+    for npi_col in NPI_COLS:
+        hist_df.update(hist_df.groupby('GeoID')[npi_col].ffill().fillna(0))
+
+    # Load model
+    with open('model.pkl', 'rb') as model_file:
+        model = pickle.load(model_file)
+
+    # Make predictions for each country,region pair
+    geo_pred_dfs = []
+    for g in ips_df.GeoID.unique():
+        print('\nPredicting for', g)
+
+        # Pull out all relevant data for country c
+        hist_gdf = hist_df[hist_df.GeoID == g]
+        ips_gdf = ips_df[ips_df.GeoID == g]
+        past_cases = np.array(hist_gdf[CASES_COL])
+        past_npis = np.array(hist_gdf[NPI_COLS])
+        future_npis = np.array(ips_gdf[NPI_COLS])
+
+        # Make prediction for each day
+        geo_preds = []
+        nb_days_to_predict = (end_date - start_date).days + 1
+        for days_ahead in range(nb_days_to_predict):
+            # Prepare data
+            X_cases = past_cases[-NB_LOOKBACK_DAYS:]
+            X_npis = past_npis[-NB_LOOKBACK_DAYS:]
+            X = np.concatenate([X_cases.flatten(),
+                                X_npis.flatten()])
+
+            # Make the prediction (reshape so that sklearn is happy)
+            pred = model.predict(X.reshape(1, -1))[0]
+            pred = max(0, pred)  # Do not allow predicting negative cases
+            geo_preds.append(pred)
+            print(pred)
+
+            # Append the prediction and npi's for next day
+            # in order to rollout predictions for further days.
+            past_cases = np.append(past_cases, pred)
+            past_npis = np.append(past_npis, future_npis[days_ahead:days_ahead + 1], axis=0)
+
+        # Create geo_pred_df with pred column
+        geo_pred_df = ips_gdf[ID_COLS].copy()
+        geo_pred_df['PredictedDailyNewCases'] = geo_preds
+        geo_pred_dfs.append(geo_pred_df)
+
+    # Combine all predictions into a single dataframe
+    pred_df = pd.concat(geo_pred_dfs)
+
+    # Drop GeoID column to match expected output format
+    pred_df = pred_df.drop(columns=['GeoID'])
+    return pred_df
 
 
 # !!! PLEASE DO NOT EDIT. THIS IS THE OFFICIAL COMPETITION API !!!
