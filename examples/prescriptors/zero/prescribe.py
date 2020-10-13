5b36ce09384a0147f5e971ea0107cfc7ce175705
ekmeyerson <elliotmeyerson@gmail.com>
2020-10-12 17:28:32 -0700

    working prescribe with api for zero prescriptor

--- examples/prescriptors/zero/prescribe.py
+++ examples/prescriptors/zero/prescribe.py
@@ -1,13 +1,61 @@
+import os
 import argparse
+import pandas as pd
 
 
+NPI_COLS = ['C1_School closing',
+            'C2_Workplace closing',
+            'C3_Cancel public events',
+            'C4_Restrictions on gatherings',
+            'C5_Close public transport',
+            'C6_Stay at home requirements',
+            'C7_Restrictions on internal movement',
+            'C8_International travel controls',
+            'H1_Public information campaigns',
+            'H2_Testing policy',
+            'H3_Contact tracing']
 
-def prescribe(start_date: str,
-              end_date: str,
+
+def prescribe(start_date_str: str,
+              end_date_str: str,
               path_to_hist_file: str,
               output_file_path) -> None:
-    return
 
+    # Create skeleton df with one row for each geo for each day
+    hdf = pd.read_csv(path_to_hist_file,
+                      parse_dates=['Date'],
+                      encoding="ISO-8859-1",
+                      error_bad_lines=True)
+    start_date = pd.to_datetime(start_date_str, format='%Y-%m-%d')
+    end_date = pd.to_datetime(end_date_str, format='%Y-%m-%d')
+    country_names = []
+    region_names = []
+    dates = []
+
+    for country_name in hdf['CountryName'].unique():
+        cdf = hdf[hdf['CountryName'] == country_name]
+        for region_name in cdf['RegionName'].unique():
+            for date in pd.date_range(start_date, end_date):
+                country_names.append(country_name)
+                region_names.append(region_name)
+                dates.append(date.strftime("%Y-%m-%d"))
+
+    prescription_df = pd.DataFrame({
+        'CountryName': country_names,
+        'RegionName': region_names,
+        'Date': dates})
+
+    # Fill df with all zeros
+    for npi_col in NPI_COLS:
+        prescription_df[npi_col] = 0
+
+    # Create the output path
+    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+
+    # Save to a csv file
+    prescription_df.to_csv(output_file_path, index=False)
+
+    return
 
 
 if __name__ == '__main__':
