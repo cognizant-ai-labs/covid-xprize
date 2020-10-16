abc3df765d3f2cc4696db5f744c3d405eb95a846
Olivier Francon <olivier.francon@cognizant.com>
2020-10-17 01:20:55 +0200

    #76 Check scenario generation for different number of countries

--- validation/tests/test_scenario_generator.py
+++ validation/tests/test_scenario_generator.py
@@ -26,14 +26,36 @@ def _get_dataset():
 
 class TestScenarioGenerator(unittest.TestCase):
 
-    def test_generate_scenario(self):
+    def test_generate_scenario_historical(self):
         latest_df = _get_dataset()
         start_date_str = "2020-08-01"
         end_date_str = "2020-08-4"
         countries = ["Italy"]
         scenario_df = generate_scenario(start_date_str, end_date_str, latest_df, countries)
         self.assertIsNotNone(scenario_df)
-        self.assertEqual(1, len(scenario_df.CountryName.unique()), "Expected only 1 country")
-        self.assertEqual("Italy", scenario_df.CountryName.unique()[0], "Not the requested country")
+        # Misleading name but checks the elements, regardless of order
+        self.assertCountEqual(countries, scenario_df.CountryName.unique(), "Not the requested countries")
         # Inception is 1/1/2020
         self.assertEqual(217, len(scenario_df), "Expected the number of days between inception and end date")
+
+        # Check multiple countries
+        countries = ["France", "Italy"]
+        scenario_df = generate_scenario(start_date_str, end_date_str, latest_df, countries)
+        self.assertIsNotNone(scenario_df)
+        # Misleading name but checks the elements, regardless of order
+        self.assertCountEqual(countries, scenario_df.CountryName.unique(), "Not the requested countries")
+        # Inception is 1/1/2020
+        self.assertEqual(217*2, len(scenario_df), "Expected the number of days between inception and end date")
+
+        # All countries: do not pass a countries list
+        scenario_df = generate_scenario(start_date_str, end_date_str, latest_df)
+        self.assertIsNotNone(scenario_df)
+        # Misleading name but checks the elements, regardless of order
+        self.assertCountEqual(latest_df.CountryName.unique(), scenario_df.CountryName.unique(),
+                              "Not the requested countries")
+        # Inception is 1/1/2020
+        # Contains the regions too. -1 to remove the NaN region, already counted as a country
+        nb_geos = len(latest_df.CountryName.unique()) + len(latest_df.RegionName.unique()) - 1
+        self.assertEqual(217*nb_geos,
+                         len(scenario_df),
+                         "Expected the number of days between inception and end date")
