a3fa35ab2917d1973cd08c9bed7046ed22a36e67
Mario Graff <mgraffg@ieee.org>
2020-12-08 08:49:40 -0600

    Creando un .py para entrenar

---
+++ covid_xprize/examples/predictors/linear/train.py
@@ -0,0 +1,120 @@
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import train_test_split
+
+# Main source for the training data
+DATA_URL = 'https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv'
+# Local file
+DATA_FILE = 'data/OxCGRT_latest.csv'
+
+import os
+import urllib.request
+if not os.path.exists('data'):
+    os.mkdir('data')
+urllib.request.urlretrieve(DATA_URL, DATA_FILE)
+
+# Load historical data from local file
+df = pd.read_csv(DATA_FILE, 
+                 parse_dates=['Date'],
+                 encoding="ISO-8859-1",
+                 dtype={"RegionName": str,
+                        "RegionCode": str},
+                 error_bad_lines=False)
+
+# For testing, restrict training data to that before a hypothetical predictor submission date
+# vamos a predecir del 20 de nov al 5 de diciembre
+HYPOTHETICAL_SUBMISSION_DATE = np.datetime64("2020-11-19")
+df = df[df.Date <= HYPOTHETICAL_SUBMISSION_DATE]                 
+
+# Add RegionID column that combines CountryName and RegionName for easier manipulation of data
+df['GeoID'] = df['CountryName'] + '__' + df['RegionName'].astype(str)
+
+# Add new cases column
+df['NewCases'] = df.groupby('GeoID').ConfirmedCases.diff().fillna(0)
+
+# Keep only columns of interest
+id_cols = ['CountryName',
+           'RegionName',
+           'GeoID',
+           'Date']
+cases_col = ['NewCases']
+npi_cols = ['C1_School closing',
+            'C2_Workplace closing',
+            'C3_Cancel public events',
+            'C4_Restrictions on gatherings',
+            'C5_Close public transport',
+            'C6_Stay at home requirements',
+            'C7_Restrictions on internal movement',
+            'C8_International travel controls',
+            'H1_Public information campaigns',
+            'H2_Testing policy',
+            'H3_Contact tracing',
+            'H6_Facial Coverings']
+df = df[id_cols + cases_col + npi_cols]
+
+# Fill any missing case values by interpolation and setting NaNs to 0
+df.update(df.groupby('GeoID').NewCases.apply(
+    lambda group: group.interpolate()).fillna(0))
+
+# Fill any missing NPIs by assuming they are the same as previous day
+for npi_col in npi_cols:
+    df.update(df.groupby('GeoID')[npi_col].ffill().fillna(0))
+
+# Set number of past days to use to make predictions
+nb_lookback_days = 30
+
+# Create training data across all countries for predicting one day ahead
+X_cols = cases_col + npi_cols
+y_col = cases_col
+X_samples = []
+y_samples = []
+geo_ids = df.GeoID.unique()
+for g in geo_ids:
+    gdf = df[df.GeoID == g]
+    all_case_data = np.array(gdf[cases_col])
+    all_npi_data = np.array(gdf[npi_cols])
+
+    # Create one sample for each day where we have enough data
+    # Each sample consists of cases and npis for previous nb_lookback_days
+    nb_total_days = len(gdf)
+    for d in range(nb_lookback_days, nb_total_days - 1):
+        X_cases = all_case_data[d-nb_lookback_days:d]
+
+        # Take negative of npis to support positive
+        # weight constraint in Lasso.
+        X_npis = -all_npi_data[d - nb_lookback_days:d]
+
+        # Flatten all input data so it fits Lasso input format.
+        X_sample = np.concatenate([X_cases.flatten(),
+                                   X_npis.flatten()])
+        y_sample = all_case_data[d + 1]
+        X_samples.append(X_sample)
+        y_samples.append(y_sample)
+
+X_samples = np.array(X_samples)
+y_samples = np.array(y_samples).flatten()
+
+# Create and train Lasso model.
+# Set positive=True to enforce assumption that cases are positively correlated
+# with future cases and npis are negatively correlated.
+model = Lasso(alpha=0.1,
+              precompute=True,
+              max_iter=10000,
+              positive=True,
+              selection='random')
+# Fit model
+model.fit(X_samples, y_samples)
+
+
+# Save model to file
+if not os.path.exists('models'):
+    os.mkdir('models')
+with open('models/model.pkl', 'wb') as model_file:
+    pickle.dump(model, model_file)
+
+
+
+
+
