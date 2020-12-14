94c21d0c14e43f698f3cd864e93b7a26855112a5
plablo09 <pablo.lopez@centrogeo.edu.mx>
2020-12-14 16:33:44 -0600

    Helpers return dataframes.

--- covid_xprize/nixtamalai/helpers.py
+++ covid_xprize/nixtamalai/helpers.py
@@ -141,23 +141,25 @@ def hampel(vals_orig, k=7, threshold=3):
     return(vals)
 
 
-def preprocess_npi(df: pd.DataFrame)->None:
+def preprocess_npi(df: pd.DataFrame):
     # Add GeoID
     df['GeoID'] = df['CountryName'] + '__' + df['RegionName'].astype(str)
     # Missing data in NPIs assuming they are the same as previous day
     for npi_col in NPI_COLS:
         df.update(df.groupby('GeoID')[npi_col].ffill().fillna(0))
+    return df
 
 
-def preprocess_newcases(df: pd.DataFrame)->None:
+def preprocess_newcases(df: pd.DataFrame):
     # Add NewCases
     df['NewCases'] = df.groupby('GeoID').ConfirmedCases.diff().fillna(0)
     # Missing data in NewCases
     df.update(df.groupby('GeoID').NewCases.apply(
         lambda group: group.interpolate()).fillna(0))
+    return df
 
 
-def preprocess(k=7, threshold=3, merge_owd='imputed'):
+def preprocess_full(k=7, threshold=3, merge_owd='imputed'):
     """Preprocess OxCGRT data.
     - Update data and merge with tests
     - Add CountryID
@@ -181,8 +183,9 @@ def preprocess(k=7, threshold=3, merge_owd='imputed'):
     """
     # get updated data merged with tests
     df = get_OxCGRT_tests()
-    preprocess_npi(df)
-    preprocess_newcases(df)
+    df = (df.pipe(preprocess_npi)
+            .pipe(preprocess_newcases)
+        )
     # Missing data in Tests
     tests_columns = [c for c in df.columns if c.startswith('tests')]
     for column in tests_columns:
