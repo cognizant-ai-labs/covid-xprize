41f98184cc50857e461926f9cf3ee6c5ec3b658a
darren.sargent <darren.sargent@cognizant.ai>
2020-12-19 18:10:30 -0800

    Trying to fix setup

--- setup.py
+++ setup.py
@@ -55,8 +55,7 @@ setup(
             'LICENSE.md'
         ]
     },
-    data_files=[('.',
-                 ['countries_regions.csv'])],
+    data_files=[('covid_xprize/data/', ['countries_regions.csv'])],
     install_requires=[
         'keras==2.4.3',
         'neat-python==0.92',
